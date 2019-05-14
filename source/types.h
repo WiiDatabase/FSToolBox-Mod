11893015c67ecdf5f70873b1773667404117460b
Andreas Bielawski <brawl98@outlook.com>
2019-05-14 19:53:49 +0200

    Upload original source code
    From https://code.google.com/archive/p/fstoolbox-ahbprot-mod/

---
+++ source/types.h
@@ -0,0 +1,18 @@
+#ifndef __TYPEDEFS_H_
+#define __TYPEDEFS_H_
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long long s64;
+
+typedef float f32;
+typedef double f64;
+
+typedef char fourcc[4];
+
+#endif //__TYPEDEFS_H_
